410c573e9bb1e0605f7b10ccefaf795b468dcead
Tyler-A <Tyler.Akins1999@gmail.com>
2019-08-27 19:15:07 -0600

    Add a bunch of stuff.

--- utils.js
+++ utils.js
@@ -1,7 +1,10 @@
 const toggle_visibility = (element_id) => {
+
     // Get the element requested
     let element = document.getElementById(element_id);
     console.log(element)
+
+
     // Confirm if the element is hidden or not, then toggle state
     if (element.getAttribute("data-hidden") === "false") {
         element.setAttribute("data-hidden", "true")
@@ -9,3 +12,6 @@ const toggle_visibility = (element_id) => {
         element.setAttribute("data-hidden", "false")
     }
 }
+
+
+const alert_webhook = (webhook_id, webhook_key) => {}
