cf772e84d74b7432d2bbcfeb4d923eaeafad4b04
Tyler-A <Tyler.Akins1999@gmail.com>
2020-06-19 01:07:04 -0600

    Remove testing data from the Vue object.

--- template/data.js
+++ template/data.js
@@ -28,40 +28,10 @@ let map_data = new Vue({
 		map: {
 			name: `MAP NAME`,
 			description: ``,
-			versions: [
-				{
-					id: `0.1.0`,
-					latest: true,
-					filename: ``,
-					mc_versions: `1.14.2 - 1.16rc1`,
-					release_date: `2020-06-18`,
-					changes: [
-						`Add support for 1.16rc1`,
-						`Fixes a bug`,
-						`Fixes another bug`,
-						`Fixes a third bug`
-					]
-				},
-				{
-					id: `0.0.1`,
-					latest: false,
-					filename: ``,
-					mc_versions: `1.14.2`,
-					release_date: `2018-05-01`,
-					changes: [
-						`Add support for 1.14.2`
-					]
-				}
-			],
+			versions: [],
 			images: [],
 			attributions: [],
-			other_downloads: [
-				{
-					name: `Custom Texture Pack`,
-					filename: ``,
-					info: ``
-				}
-			]
+			other_downloads: []
 		},
 		page: {
 			show: layout,
