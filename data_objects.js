37e79f135b5d7d0410dca208954b61d61a71570e
Tyler-A <Tyler.Akins1999@gmail.com>
2020-06-21 17:38:48 -0600

    Add example data objects for the Vue objects.

---
+++ data_objects.js
@@ -0,0 +1,29 @@
+const version_object = {
+	id: ``,
+	latest: true,
+	flagged: {
+		show: false,
+		colour: ``
+	},
+	hidden: true,
+	download: false,
+	filename: ``,
+	mc_versions: ``,
+	release_date: ``,
+	changes: [``]
+};
+
+
+const attribution = {
+	name: `Jarren`,
+	info: ``,
+	links: [
+		[`Text`, `Link`]
+	]
+}
+
+
+const image_object = [
+	`filename`,
+	`alt text`
+]
